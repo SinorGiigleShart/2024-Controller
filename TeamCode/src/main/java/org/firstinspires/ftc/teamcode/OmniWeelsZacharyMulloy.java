39857acd30a5205fbd5ff9b4d8664c4fe50fc094
SinorGiigleShart <mulloyzachary@gmail.com>
2024-09-25 14:26:53 -0700

    Update OmniWeelsZacharyMulloy.java
    i added the hardware map to the motors and a new servo as well as the other thingy that's not the hardware map but its the other thing that you need to properly delcare a motor/servo.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniWeelsZacharyMulloy.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniWeelsZacharyMulloy.java
@@ -1,3 +1,38 @@
+/* Copyright (c) 2021 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /*
@@ -28,14 +63,14 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@TeleOp(name="OmniWheels 0.1", group="OmniOp")
+@TeleOp(name="OmniWeelsZacharyMulloy 0.1", group="OmniOp")
 @Disabled
-public class OmniWheels extends LinearOpMode {
+public class OmniWeelsZacharyMulloy extends LinearOpMode {
 
     // Declare OpMode members for each of the 4 motors.
     private ElapsedTime runtime = new ElapsedTime();
     
-        //driving motors 
+        //driving motors/Not Zac's motors  
     private DcMotor leftFrontDrive = null;
     private DcMotor rightFrontDrive = null;
     private DcMotor leftBackDrive = null;
@@ -44,16 +79,29 @@ public class OmniWheels extends LinearOpMode {
         //suction tube Up motor arms/Zac's motors
     private DcMotor tubelifter = null;
     private DcMotor impeller = null;
+    
+        //servo for no reason just for funsies 
+    private Servo thisIsAServo = null;
   
     @Override
     public void runOpMode() {
 
         // Initialize the hardware variables. Note that the strings used here must correspond
         // to the names assigned during the robot configuration step on the DS or RC devices.
+
+
+            //not Zac's motots 
         leftFrontDrive  = hardwareMap.get(DcMotor.class, "left_front_drive");
         leftBackDrive  = hardwareMap.get(DcMotor.class, "left_back_drive");
         rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
         rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
+
+            //Zac's motors
+        tubelifter = hardwareMap.get(DcMotor.class, "tube_lifter");
+        impeller = hardwareMap.get(DcMotor.class, "_impeller_");
+
+            //Zac's servos
+        thisIsAServo = hardwareMap.get(Servo.class, "this_Is_A_Servo"); 
 
         // ########################################################################################
         // !!!            IMPORTANT Drive Information. Test your motor directions.            !!!!!
@@ -65,14 +113,23 @@ public class OmniWheels extends LinearOpMode {
         // When you first test your robot, push the left joystick forward and observe the direction the wheels turn.
         // Reverse the direction (flip FORWARD <-> REVERSE ) of any wheel that runs backward
         // Keep testing until ALL the wheels move the robot forward when you push the left joystick forward.
+
+            //not Zac's motors 
         leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
         leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
 
+            //Zac's motors
+        tubelifter.setDirection(DcMotor.Direction.FORWARD);
+        impeller.setDirection(DcMotor.Direction.FORWARD);
 
+            //Zac's servos
+        thisIsAServo.setDirection(Servo.Direction.FORWARD);
+        
 
         // Wait for the game to start (driver presses START)
+        
         telemetry.addData("High Five", "We Reboted!!!");
         telemetry.update();
 
@@ -80,10 +137,12 @@ public class OmniWheels extends LinearOpMode {
         runtime.reset();
 
         // run until the end of the match (driver presses STOP)
+        
         while (opModeIsActive()) {
             double max;
 
             // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
+            
             double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value -- Forward/Backward
             double lateral =  gamepad1.right_stick_x;  // Strafe left/right
             double yaw     =  gamepad1.left_stick_x; // Rotate left/right
@@ -91,6 +150,7 @@ public class OmniWheels extends LinearOpMode {
 
             // Combine the joystick requests for each axis-motion to determine each wheel's power.
             // Set up a variable for each drive wheel to save the power level for telemetry.
+            
             double leftFrontPower  = axial + lateral + yaw;
             double rightFrontPower = axial - lateral - yaw;
             double leftBackPower   = axial - lateral + yaw;
@@ -98,6 +158,7 @@ public class OmniWheels extends LinearOpMode {
 
             // Normalize the values so no wheel power exceeds 100%
             // This ensures that the robot maintains the desired motion.
+            
             max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
             max = Math.max(max, Math.abs(leftBackPower));
             max = Math.max(max, Math.abs(rightBackPower));
@@ -139,5 +200,4 @@ public class OmniWheels extends LinearOpMode {
             telemetry.update();
         }
     }}
-
 
